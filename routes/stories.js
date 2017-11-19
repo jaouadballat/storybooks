a369ebf4b7218cb7cd912d9545e989a802376164
jaouad ballat <jaouad.ballat@gmail.com>
2017-11-19 11:53:49 +0000

    add edit and update form

--- routes/stories.js
+++ routes/stories.js
@@ -4,12 +4,58 @@ var auth = require('../config/auth');
 var Story = require('../models/story');
 
 /* GET home page. */
-router.get('/dashboard',auth, function(req, res, next) {
-  res.render('dashboard');
+router.get('/public', function(req, res, next) {
+	Story.find({status: 'public'}).populate("user").exec(function(err, stories){
+		if(err){
+			console.log(err)
+		}else{
+		  res.render('public_stories', {
+		  	stories: stories
+		  });
+		}
+	})
+});
+
+router.get('/dashboard',auth, function(req,res){
+	console.log(req.user._id)
+	Story.find({}).where('user').equals(req.user).exec(function(err, stories){
+		if(err){
+			console.log(err)
+		}else{
+			res.render('dashboard', {
+				stories: stories
+
+			});
+		}
+	});
+});
+
+router.get('/edit/:id',auth, function(req, res){
+	Story.findById(req.params.id).exec(function(err, story){
+		if(err){
+			console.log(err)
+		}else{
+			res.render("edit_story", {
+				story: story
+			});
+		}
+	});
 });
 
 router.get('/add', function(req, res, next) {
   res.render('add_story');
+});
+
+router.get('/show/:id', function(req, res){
+	Story.findById(req.params.id).populate('user').exec(function(err, story){
+		if(err){
+			console.log(err)
+		}else{
+			res.render('show', {
+				story: story
+			});
+		}
+	});
 });
 
 router.post('/', function(req, res, next) {
@@ -35,4 +81,32 @@ router.post('/', function(req, res, next) {
 		}
 	});
 });
+
+router.post('/update/:id',auth, function(req, res){
+	Story.findById(req.params.id, function(err, story){
+		if(err){
+			console.log(err)
+		}else{
+			let allowComments;
+			if (req.body.allowComments) {
+			    allowComments = true
+			} else {
+			    allowComments = false
+			}
+			story.title = req.body.title,
+		    story.status = req.body.status,
+		    story.body = req.body.body,
+		    story.allowcomments = allowComments
+		    story.save(function(err, story){
+		    	if(err){
+		    		console.log(err)
+		    	}else{
+					res.redirect("/stories/dashboard")
+		    	}
+		    })
+		}
+
+	});
+});
+
 module.exports = router;
